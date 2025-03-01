ee2d07100d4571f2cb77deaa92e59f5dff80c384
xiwilson <linzi.wilson@hotmail.com>
2025-03-01 22:49:12 +0000

    Added basic JS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "3f47deba7d7o337tb76d054056f8cbe1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axois.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
