fa6bd4a61941102a63012bff52bfee4ddcfa5bfa
xiwilson <linzi.wilson@hotmail.com>
2025-03-02 12:40:37 +0000

    Added in temperature value

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 function refreshWeather(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "3f47deba7d7o337tb76d054056f8cbe1";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axois.get(apiUrl).then(refreshWeather);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
