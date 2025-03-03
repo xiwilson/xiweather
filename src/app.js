8cec6bc1a5cf10c5c40533bb95cf66c14fdc8f84
xiwilson <linzi.wilson@hotmail.com>
2025-03-03 12:48:12 +0000

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,40 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   temperatureElement.innerHTML = response.data.temperature.current;
+  timeElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
