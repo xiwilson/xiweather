654aa56d2529f92f6ad0c6eeba36d7b4974e5591
xiwilson <linzi.wilson@hotmail.com>
2025-03-03 22:45:43 +0000

    CSS updated

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
+  let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = response.data.temperature.current;
   timeElement.innerHTML = formatDate(date);
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
@@ -48,10 +50,36 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
+}
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecast +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤</div>
+    <div class="weather-forecast-temperature">
+      <div class="weather-forecast-temperature-unit"></div>
+      <strong>15°</strong>
+      <div class="weather-forecast-temperature-unit">9°</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
+displayForecast();
